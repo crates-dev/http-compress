d7d19b13e118c155508ae36c9438f462367f5029
尤雨东 <root@ltpp.vip>
2024-12-22 08:35:02 +0800

    feat:mod

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
-mod brotli;
-mod cfg;
-mod compress;
-mod deflate;
-mod gzip;
+pub(crate) mod brotli;
+pub(crate) mod cfg;
+pub(crate) mod compress;
+pub(crate) mod deflate;
+pub(crate) mod gzip;
 
 pub use compress::r#type::*;
