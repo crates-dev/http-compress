6906d90aca131c17a45ca908df3c192773b64892
尤雨东 <root@ltpp.vip>
2025-02-04 10:29:32 +0800

    feat:

--- sh/init.sh
+++ sh/init.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-git remote add jihulab ssh://jihulab.com:ltpp/http-compress.git;
+git remote add jihulab git@jihulab.com:ltpp-universe/http-compress.git;
 git remote add origin git@github.com:ltpp-universe/http-compress.git;
