01c5a894c94a127f3b3afe4cc1272cdff5d91a51
尤雨东 <root@ltpp.vip>
2025-03-21 07:55:27 +0800

    feat: v2.4.0

--- src/lib.rs
+++ src/lib.rs
@@ -8,15 +8,15 @@ pub use compress::r#type::*;
 
 pub(crate) use ::brotli::Decompressor;
 pub(crate) use flate2::{
+    Compression,
     read::{DeflateDecoder, GzDecoder},
     write::{DeflateEncoder, GzEncoder},
-    Compression,
 };
 pub(crate) use http_constant::*;
 pub(crate) use std::{
     borrow::Cow,
     collections::HashMap,
     fmt,
-    io::{prelude::*, BufReader, BufWriter, Read},
+    io::{BufReader, BufWriter, Read, prelude::*},
     str::FromStr,
 };
