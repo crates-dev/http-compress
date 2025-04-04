63dd9a4e9600141fe5b290024440602a42670c63
尤雨东 <root@ltpp.vip>
2025-04-04 08:55:17 +0800

    feat: v2.11.0

---
+++ src/compress/constant.rs
@@ -0,0 +1,5 @@
+pub static CONTENT_ENCODING: &str = "content-encoding";
+pub static CONTENT_ENCODING_GZIP: &str = "gzip";
+pub static CONTENT_ENCODING_DEFLATE: &str = "deflate";
+pub static CONTENT_ENCODING_BROTLI: &str = "br";
+pub static EMPTY_STR: &str = "";
--- src/lib.rs
+++ src/lib.rs
@@ -8,13 +8,13 @@ pub use compress::r#type::*;
 pub use twox_hash::XxHash3_64;
 
 pub(crate) use ::brotli::Decompressor;
+pub(crate) use compress::constant::*;
 pub(crate) use core::hash::BuildHasherDefault;
 pub(crate) use flate2::{
     Compression,
     read::{DeflateDecoder, GzDecoder},
     write::{DeflateEncoder, GzEncoder},
 };
-pub(crate) use http_constant::*;
 pub(crate) use std::{
     borrow::Cow,
     collections::HashMap,
