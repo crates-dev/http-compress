d9545567dcf1437bbdc4b7c3afb8f6de7021bc9d
尤雨东 <root@ltpp.vip>
2025-12-28 11:21:21 +0800

    feat: v3.0.6

--- src/compress/type.rs
+++ src/compress/type.rs
@@ -1,5 +1,5 @@
 /// Represents different compression algorithms supported by the library.
-#[derive(Debug, Default, PartialEq, Eq)]
+#[derive(Debug, Default, PartialEq, Eq, Clone, Copy)]
 pub enum Compress {
     /// Gzip compression algorithm.
     Gzip,
